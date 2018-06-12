d2bd35d7831234a3fdf2f0eb0fbfd7b1acf6287f
Rith <rithronlkh@gmail.com>
2018-06-12 15:20:49 +0700

    refector userservice impl from super to add autowired userMapper

--- src/main/java/com/aidijing/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/aidijing/user/service/impl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.aidijing.user.domain.User;
 import com.aidijing.user.mapper.UserMapper;
 import com.aidijing.user.service.UserService;
 import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,15 +21,17 @@ import java.util.List;
 @Service
 public class UserServiceImpl extends ServiceImpl< UserMapper, User > implements UserService {
 
+    @Autowired
+    UserMapper userMapper;
 
     @Override
     public List< User > list () {
-        return super.selectList( null );
+        return userMapper.selectList( null );
     }
 
     @Override
     public User save ( User user ) {
-        super.insert( user );
+        userMapper.insert( user );
         System.err.println("Save");
         return user;
     }
